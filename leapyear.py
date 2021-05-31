34bec34e74f5ca5e6e91f5117b0e6465015fb2c9
hannahmaung <72160088+hannahmaung@users.noreply.github.com>
2021-05-31 13:48:47 -0700

    Finished leap year program
    finds what year is a leap year.

--- leapyear.py
+++ leapyear.py
@@ -1,6 +1,20 @@
 
 #Leap Year program 
 
-def test():
-    pass
+def test(year):
+    
+    ##If divisible by 4
+    if (year % 4) == 0:
+        ##If divisible by 100
+        if (year % 100) == 0:
+            #If divisible by 400
+            if (year % 400) == 0:
+                return True
+            else:
+                return False     
+        else:
+            return True
+    else:
+        return False 
+
 
