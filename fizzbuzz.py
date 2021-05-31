3332c271268b195fae777bee98caf87b1f89a6b8
hannahmaung <72160088+hannahmaung@users.noreply.github.com>
2021-05-31 13:29:53 -0700

    Finished fizzbuzz program.
    For range 1-100, the program prints out "Fizz" for multiples of three, prints out "Buzz" for multiples of five, and prints out "FizzBuzz" for multiples of both three and five.

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,5 +1,20 @@
 
 #Fizzbuzz program 
 
-def test():
-    pass
+def test(fizzbuzz):
+
+    # if number is a multiple of both 3 and 5, print FizzBuzz
+    if fizzbuzz % 3 == 0 and fizzbuzz % 5 == 0:
+        return("FizzBuzz")
+        
+    # if number is a multiple of 3 print Fizz
+    elif fizzbuzz % 3 == 0:
+        return("Fizz")
+        
+    # if number is a multiple of 5 print Buzz
+    elif fizzbuzz % 5 == 0:
+        return("Buzz")
+
+    # else, return fizzbuzz   
+    else:
+        return(fizzbuzz)
