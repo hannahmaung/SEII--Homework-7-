b74306034a4ab9cb50d1a73bace86472e83d8e21
hannahmaung <72160088+hannahmaung@users.noreply.github.com>
2021-05-31 13:32:17 -0700

    Implemented unit test method, test 2 passes
    Test 2 will pass, 15 is a multiple of both 3 and 5 which prints out "FizzBuzz"

--- fizzbuzz_unittest.py
+++ fizzbuzz_unittest.py
@@ -4,10 +4,14 @@ import unittest
 from fizzbuzz import test # type: ignore
 
 class testCase(unittest.TestCase):
-    #Test 1 will pass 
+    #Test 1 will pass, 5 is a multiple of 5, which prints out Buzz
     def test1(self):
         for input in [5]:
             assert test(input) == 'Buzz'
+    #Test 2 will pass, 15 is a multiple of both 3 and 5 which prints out "FizzBuzz"
+    def test2(self):
+        for input in [15]:
+            assert test(input) == "FizzBuzz"
 
 if __name__ == "__main__":
         unittest.main()
