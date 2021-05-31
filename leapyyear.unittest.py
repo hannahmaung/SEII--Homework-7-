88f8163bf3c607d6e009362e073698fb74c6bfbf
hannahmaung <72160088+hannahmaung@users.noreply.github.com>
2021-05-31 13:52:44 -0700

    Implemented unit test method, test 3 passes.
    Test 3 will pass because 2001 is not a leap year, unit test result is set to False.

--- leapyyear.unittest.py
+++ leapyyear.unittest.py
@@ -13,6 +13,10 @@ class testCase(unittest.TestCase):
     def test2(self):
         for input in [2000]:
             assert test(input) == True
+    # Test 2 will pass, 2001 is not a leap year
+    def test3(self):
+        for input in [2001]:
+            assert test(input) == False
 
 if __name__ == "__main__":
         unittest.main()
